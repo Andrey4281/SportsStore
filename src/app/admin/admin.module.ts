387c6fd625351fd4b6e133acd272fb1451dccaf0
Andrey Semenov <Andrey4281@yandex.ru>
2020-03-07 16:46:40 +0500

    Version 1.0

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,16 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { AdminComponent } from './admin.component';
+import {AuthGuard} from './auth.guard';
+import {ProductEditorComponent} from './productEditor.component';
+import {ProductTableComponent} from './productTable.component';
+import {OrderTableComponent} from './orderTable. component';
 
 let routing = RouterModule.forChild([
-  {path : "auth", component: AuthComponent},
-  {path: "main", component: AdminComponent},
-  {path: "**", redirectTo: "auth"}
+  { path: "auth", component: AuthComponent },
+  {
+    path: "main", component: AdminComponent, canActivate: [AuthGuard],
+    children: [
+      { path: "products/:mode/:id", component: ProductEditorComponent },
+      { path: "products/:mode", component: ProductEditorComponent },
+      { path: "products", component: ProductTableComponent },
+      { path: "orders", component: OrderTableComponent },
+      { path: "**", redirectTo: "products" }
+    ]
+  },
+  { path: "**", redirectTo: "auth" }
 ]);
 
 @NgModule({
   imports: [CommonModule, FormsModule, routing],
-  declarations: [AuthComponent, AdminComponent]
+  declarations: [AuthComponent, AdminComponent, ProductEditorComponent,
+    ProductTableComponent, OrderTableComponent],
+  providers: [AuthGuard]
 })
 export class AdminModule { }
 
--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -8,6 +8,9 @@ import {map} from 'rxjs/operators';
 const PROTOCOL = "http";
 const PORT = 3500;
 
+/**
+ * Рест клиент для получения данных
+ */
 @Injectable({ providedIn: 'root' })
 export class RestDataSource {
   baseUrl: string;
@@ -34,5 +37,29 @@ export class RestDataSource {
   saveOrder(order : Order) : Observable<Order> {
     return this.http.post<Order>(this.baseUrl + 'orders', order,
       {headers: {'authorization': `Bearer<${this.auth_token}>`}});
+  }
+
+  saveProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl + 'products', product, {headers: {'authorization': `Bearer<${this.auth_token}>`}});
+  }
+
+  updateProduct(product : Product): Observable<Product> {
+    return this.http.put<Product>(this.baseUrl + `products/${product.id}`, product, {headers: {'authorization': `Bearer<${this.auth_token}>`}});
+  }
+
+  deleteProduct(id: number): Observable<Product> {
+    return this.http.delete<Product>(this.baseUrl + `products/${id}`, {headers: {'authorization': `Bearer<${this.auth_token}>`}});
+  }
+
+  getOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.baseUrl + 'orders', {headers: {'authorization': `Bearer<${this.auth_token}>`}});
+  }
+
+  deleteOrder(id: number): Observable<Order> {
+    return this.http.delete<Order>(this.baseUrl + `orders/${id}`, {headers: {'authorization': `Bearer<${this.auth_token}>`}});
+  }
+
+  updateOrder(order: Order): Observable<Order> {
+    return this.http.put<Order>(this.baseUrl + `orders/${order.id}`, order, {headers: {'authorization': `Bearer<${this.auth_token}>`}});
   }
 }
